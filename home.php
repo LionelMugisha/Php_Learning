f85d1c0db92754065d2d09ba7fbbbc0dfcdf0fbe
LionelMugisha <lionelmugisha2@gmail.com>
2021-12-28 11:41:01 +0200

    embedding html and php

--- home.php
+++ home.php
@@ -24,9 +24,20 @@
         </h1>
 
         <!-- By using one single line-->
-        <h1>
-            <?php echo "Hello " . $_GET['name']; ?>
+        <h1> 
+            <?php echo "Hello " . $_GET['name'] . ', I am ' . $_GET['age'] . " years old <br>"; ?> 
+                <!-- or -->
+            <?="Good Morning " . htmlspecialchars($_GET['name']); ?>
         </h1>
+
+        <p>
+            <?="<br> Hello Mr. <b>" . $_GET['pname'] . "</b>, I am here by telling you that at <b>" . $_GET['time'] . " </b> we
+            are going to host a meeting to welcome you to our company called <b>" . $_GET['company'] . "</b>"; ?>
+        
+        <!-- How you pass variables in the address bar 
+            http://localhost/training/Php/home.php?pname=Walter&time=3PM&company=Zatec 
+        -->    
+    </p>
 
     </header>
 
