8c03c46fb869083919e319d745aeb258e8594bca
LionelMugisha <lionelmugisha2@gmail.com>
2021-12-28 12:39:17 +0200

    looking into arrays

---
+++ array/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$names = [
+    'Jeff',
+    'Tarzan',
+    'Mary'
+];
+
+$liverpoolPlayers = [
+    'Alisson',
+    'TAA',
+    'Robertson',
+    'VVD',
+    'Matip',
+    'Fabinho',
+    'Henderson',
+    'Firmino',
+    'Salah',
+    'Mane',
+    'Jota'
+];
+
+require 'index.view.php';
