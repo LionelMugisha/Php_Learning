8e1094d398eb157df7a7313050bad5f67bfac94a
LionelMugisha <lionelmugisha2@gmail.com>
2021-12-28 13:39:29 +0200

    looking into the associative array

--- array/index.php
+++ array/index.php
@@ -20,4 +20,33 @@ $liverpoolPlayers = [
     'Jota'
 ];
 
+//associative array
+$person = [
+    'age' => 19,
+    'hair' => 'brown',
+    'career' => 'student',
+];
+$person['country'] = 'Rwandan'; //add an element in the array
+unset($person['hair']); //remove an element in the array
+
+//die(var_dump($person));  //var_dump function changes the array into a string
+//die function will stop everything that will come after that to be executed.
+
+$liverpoolPlayer = [
+    'GK' => 'Alisson',
+    'RB' => 'TAA',
+    'LB' => 'Robertson',
+    'CB' => 'VVD',
+    'CB' => 'Matip',
+    'DM' => 'Fabinho',
+    'CM' => 'Henderson',
+    'AM' => 'Firmino',
+    'RW' => 'Salah',
+    'LW' => 'Mane',
+    'P' => 'Jota',
+    'Sub' => 'Chamberlain'
+];
+$liverpoolPlayer['Coach'] = 'Jurgen Klopp';  //add an element in the associative array
+unset($liverpoolPlayer['Sub']); //delete an element in the associative array
+
 require 'index.view.php';
--- array/index.view.php
+++ array/index.view.php
@@ -37,5 +37,23 @@
             <?="<br><h3>There are $count players listed.</h3>"; ?>
         </ul>
 
+        <!-- displaying element from the associative array -->
+        <ul>
+            <h3>Personal Information:</h3>
+            <?php 
+            foreach ($person as $key => $feature) : ?>
+                <li> <strong><?= "$key = "; ?></strong><?= $feature; ?> </li>   
+            <?php endforeach; ?>
+        </ul>
+
+        <!-- displaying element from the associative array -->
+        <ul>
+            <h3>List of Liverpool Players:</h3>
+            <?php 
+            foreach ($liverpoolPlayer as $key => $players) : ?>
+                <li><strong><?= "$key = "; ?></strong><?= $players; ?> </li>   
+            <?php endforeach; ?>
+        </ul>
+
 </body>
 </html>
